e75c4ee277e8d7116af06f2191d4de4951dafb1a
Serebrennikovki <serebrennikov_k_I@mail.ru>
2022-11-12 15:35:36 +0500

    finally

--- src/components/FilterCheckbox/FilterCheckbox.js
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import './FilterCheckbox.css';
 
-function FilterCheckbox(){
+function FilterCheckbox({disabled, pressedCheckBox}){
+
     return(
         <label className='filterCheckBox'>
-            <input type='checkBox' className='filterCheckBox__checkBox'/>
+            <input type='checkBox' className='filterCheckBox__checkBox' onChange={pressedCheckBox}/>
             <span className='filterCheckBox__slider'/>
         </label>
     )
